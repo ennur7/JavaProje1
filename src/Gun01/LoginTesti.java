8b4b5910bfde102826ce8a641517700c276f0bb8
ennur7 <ennur789@gmail.com>
2025-03-02 22:36:30 +0300

    Login testi asama 1 (Jiradaki gelen task daki başlık)

---
+++ src/Gun01/LoginTesti.java
@@ -0,0 +1,13 @@
+package Gun01;
+
+public class LoginTesti {
+    public static void main(String[] args) {
+
+        // Bu bölüm Login testi için yeni şifre modeline göre yazıldı
+        System.out.println("Yeni Login Testi ");
+
+        // Çok satır yazdım ve kodları bitirdim
+        System.out.println("Login Test 1 ");
+
+    }
+}
