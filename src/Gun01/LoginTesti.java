e700a3f1557056effc9aeb6e7bbffa12cc54e950
ennur7 <ennur789@gmail.com>
2025-03-02 22:41:56 +0300

    Yeni Login testi yapıldı

--- src/Gun01/LoginTesti.java
+++ src/Gun01/LoginTesti.java
@@ -3,13 +3,14 @@ package Gun01;
 public class LoginTesti {
     public static void main(String[] args) {
 
-        // Bu bölüm Login testi için yeni şifre modeline göre yazıldı
-        System.out.println("Yeni Login Testi ");
+
 
         // Çok satır yazdım ve kodları bitirdim
         System.out.println("Login Test 1 ");
 
         System.out.println("Sepet testi yazıldı bitirildi, 3 günlük kod yazıldı");
+
+        System.out.println("Yeni Login Testi yapıldı,7 gün uğraştım");
 
     }
 }
